b77830fd41bfef9f2e0fb156134d53b8f5733664
mohamadlounnas <mohamadlounnas@gmail.com>
2024-07-25 18:56:30 +0100

    sg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "820918165"
+    serviceWorkerVersion: "3586729785"
   }
 });
