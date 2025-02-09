dc216b3f26ce0bb3283517056317f803826d433d
mohamadlounnas <mohamadlounnas@gmail.com>
2025-02-09 04:01:06 +0100

    Remove unused Flutter logo images and update various SVG assets; increment version to 1.11.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -15,16 +15,16 @@ const userConfig = renderer ? {'renderer': renderer} : {};
 
 // addversion to mainJsPath
 _flutter.buildConfig.builds.forEach(build => {
-    build.mainJsPath += "?v="+"1169943";
+    build.mainJsPath += "?v="+"1810015142";
 });
     
 _flutter.loader.load({
   config: {
     ...userConfig,
-    mainJsPath: "main.dart.js?v=" + "1169943",
+    mainJsPath: "main.dart.js?v=" + "1810015142",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1169943",
+    serviceWorkerVersion: "1810015142",
   },
   onEntrypointLoaded: function (engineInitializer) {
     engineInitializer.initializeEngine({
