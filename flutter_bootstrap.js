68f91e38b24b1fe6ef78484a68741e374258958c
mohamadlounnas <mohamadlounnas@gmail.com>
2025-04-03 05:24:00 +0100

    sg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -15,16 +15,16 @@ const userConfig = renderer ? {'renderer': renderer} : {};
 
 // addversion to mainJsPath
 _flutter.buildConfig.builds.forEach(build => {
-    build.mainJsPath += "?v="+"3519527743";
+    build.mainJsPath += "?v="+"1949154235";
 });
     
 _flutter.loader.load({
   config: {
     ...userConfig,
-    mainJsPath: "main.dart.js?v=" + "3519527743",
+    mainJsPath: "main.dart.js?v=" + "1949154235",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3519527743",
+    serviceWorkerVersion: "1949154235",
   },
   onEntrypointLoaded: function (engineInitializer) {
     engineInitializer.initializeEngine({
