9ec350b845f39e4a34a1d74822cb1de7e273789d
mohamadlounnas <mohamadlounnas@gmail.com>
2025-05-25 04:00:41 +0100

    Refactor code structure for improved readability and maintainability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -15,16 +15,16 @@ const userConfig = renderer ? {'renderer': renderer} : {};
 
 // addversion to mainJsPath
 _flutter.buildConfig.builds.forEach(build => {
-    build.mainJsPath += "?v="+"1350758777";
+    build.mainJsPath += "?v="+"32341173";
 });
     
 _flutter.loader.load({
   config: {
     ...userConfig,
-    mainJsPath: "main.dart.js?v=" + "1350758777",
+    mainJsPath: "main.dart.js?v=" + "32341173",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1350758777",
+    serviceWorkerVersion: "32341173",
   },
   onEntrypointLoaded: function (engineInitializer) {
     engineInitializer.initializeEngine({
