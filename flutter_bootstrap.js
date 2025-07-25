16166d8e6a730d75d41d320acea64ecff2b341d8
mohamadlounnas <mohamadlounnas@gmail.com>
2025-07-25 22:37:38 +0100

    deploy web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,16 +43,16 @@ const userConfig = renderer ? {'renderer': renderer} : {};
 
 // addversion to mainJsPath
 _flutter.buildConfig.builds.forEach(build => {
-    build.mainJsPath += "?v="+"297715175";
+    build.mainJsPath += "?v="+"90679434";
 });
     
 _flutter.loader.load({
   config: {
     ...userConfig,
-    mainJsPath: "main.dart.js?v=" + "297715175",
+    mainJsPath: "main.dart.js?v=" + "90679434",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "297715175",
+    serviceWorkerVersion: "90679434",
   },
   onEntrypointLoaded: function (engineInitializer) {
     engineInitializer.initializeEngine({
--- main.dart.js_3.part.js
+++ main.dart.js_3.part.js
@@ -1,9 +1,9 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B,C={
-edT(d,e){var y=null
-return A.dQP(e,B.a0d,new A.a9Q(d,y,y,y),y)}}
+eed(d,e){var y=null
+return A.dRa(e,B.a0f,new A.a9Q(d,y,y,y),y)}}
 A=c[4]
 B=c[6]
 C=a.updateHolder(c[3],C)
 var z=a.updateTypes([])};
-(a=>{a["5b1/Fs4SGn1FfEYkeTwySVaVl1A="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["dbLqqKZ+Y/QiUUF/6jxKDuWElnw="]=a.current})($__dart_deferred_initializers__);
