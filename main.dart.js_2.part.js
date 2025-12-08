baf9ec16d585030c444bb3e6cd8882f763b37180
mohamadlounnas <mohamadlounnas@gmail.com>
2025-12-08 16:27:21 +0100

    deploy web

--- main.dart.js_2.part.js
+++ main.dart.js_2.part.js
@@ -1,9 +1,9 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B,C={
-eyY(d,e){var y=null
-return A.eax(e,B.Cu,new A.r0(d,y,y,y),y)}}
+eyZ(d,e){var y=null
+return A.eay(e,B.Cu,new A.r0(d,y,y,y),y)}}
 A=c[0]
 B=c[2]
 C=a.updateHolder(c[3],C)
 var z=a.updateTypes([])};
-(a=>{a["btQhHAqjwHrc5kFA7WJjW478xsU="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["mJX64O/PqZnj8vDXmUS+w0C8V6w="]=a.current})($__dart_deferred_initializers__);
